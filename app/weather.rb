0a6eb3b4733f6352d010396312877266ffcea6dd
Andres <andres@cobogpgp1111.global.publicisgroupe.net>
2016-07-03 20:33:09 -0500

    add formatting of yahoo responses

--- app/weather.rb
+++ app/weather.rb
@@ -28,10 +28,34 @@ class Weather
       ) and u='c' limit 1
     })
 
-    return response
+    formatted_response = transform_forecast_object(
+      response.parsed_response['query']['results']['channel']
+    )
+    return formatted_response
   end
 
   private
-  def transform_forecast_object()
+  def transform_forecast_object(response)
+    forecast_response = {}
+
+    lat_lng = {
+      latitude: response['item']['lat'],
+      longitude: response['item']['long']
+    }
+    forecast_response[:units] = response['units']
+    forecast_response[:location] = response['location'].merge(lat_lng)
+
+    # get higher and lower temperatures for today
+    today_forecast = response['item']['forecast'].first
+    today_temps    = { high: today_forecast['high'], low: today_forecast['low'] }
+
+    forecast_response[:today] = {
+      wind: response['wind'],
+      atmosphere: response['atmosphere'],
+      condition: response['item']['condition'].merge(today_temps)
+    }
+    forecast_response[:week] = response['item']['forecast'].drop(1)
+
+    return forecast_response
   end
 end
